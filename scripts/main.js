abeadb016d04bb2b15c8f8fccafddeb86847ef9d
Michelle Totherow <tym4me13@gmail.com>
2023-08-10 04:32:13 -0500

    examples of different fetch struct

--- scripts/main.js
+++ scripts/main.js
@@ -7,12 +7,26 @@ fetch(`https://randomfox.ca/floof/.`)
     }
   )
 
-fetch(`http://shibe.online/api/cats?count=1&urls=true&httpsUrls=false`)
-  .then(response => response.json())
-  .then(
-    (convertedData) => {
-       const catImgElement = document.querySelector("#cat")
-       catImgElement.src = convertedData[0]
-    }
-  )
+const getTheCat = async () => {
+    const response = await fetch(`http://shibe.online/api/cats?count=1&urls=true&httpsUrls=false`)
+    const convertedData = await response.json()
+    const catImgElement = document.querySelector("#cat")
+    catImgElement.src = convertedData[0]
+}
 
+getTheCat()
+
+// fetch(`http://shibe.online/api/cats?count=1&urls=true&httpsUrls=false`)
+//   .then(response => response.json())
+//   .then(
+//     (convertedData) => {
+//        const catImgElement = document.querySelector("#cat")
+//        catImgElement.src = convertedData[0]
+//     }
+//   )
+
+
+    // const response = await fetch(`http://shibe.online/api/cats?count=1&urls=true&httpsUrls=false`)
+    // const convertedData = await response.json()
+    // const catImgElement = document.querySelector("#cat")
+    // catImgElement.src = convertedData[0]
